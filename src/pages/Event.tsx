0224c1c44c80da25a19ecb4471b85330a27ca787
Vinicius <viniholtman28@gmail.com>
2022-08-08 19:42:18 -0300

    Create query for get data lesson

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,15 +1,20 @@
+import { useParams } from 'react-router';
 import { Header } from '../components/Header';
 import { PlayerVideo } from '../components/PlayerVideo';
 import { Sidebar } from '../components/Sidebar';
 
-
 export function Event() {
+  const { slug } = useParams<{ slug: string }>()
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
       
       <main className='flex flex-1'>
-        <PlayerVideo />
+        { slug 
+          ? <PlayerVideo lessonSlug={slug} /> 
+          : <div className='flex-1'/>
+        }
         <Sidebar />
       </main>
     </div>
